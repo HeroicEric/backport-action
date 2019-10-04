0e2eae215c4e5268b022747ec29f19723b1b261c
Eric Kelly <heroiceric@gmail.com>
2019-10-03 21:41:28 -0700

    WIP

---
+++ .github/actions/backport-action/index.js
@@ -0,0 +1,49 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    let githubToken = core.getInput('github-token');
+    let octokit = new github.GitHub(githubToken);
+
+    let owner = 'HeroicEric';
+    let repo = 'backport-action';
+
+    let pr = github.context.payload.pull_request;
+
+    let targetMeBranch = await octokit.git.getRef({
+      owner,
+      repo,
+      ref: 'refs/heads/targetme'
+    });
+
+    let msg = console.log(
+      JSON.stringify(targetMeBranch, undefined, 2)
+    );
+
+    await octokit.pulls.create({
+      owner,
+      repo,
+      title: 'WoWoWoWoWoW',
+      head: pr.head.ref,
+      base: 'targetme'
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
